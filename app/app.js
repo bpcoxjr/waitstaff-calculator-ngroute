e93dc3ac407825013e2a032e0df3fe8f87c0c2d6
Bruce P Cox Jr <bpcoxjr@yahoo.com>
2016-03-11 13:54:27 -0500

    worked on controllers

--- app/app.js
+++ app/app.js
@@ -1,15 +1,14 @@
 var app = angular.module('waitStaffApp', ['ngRoute']);
 
 app.config(['$routeProvider', function($routeProvider) {
-	//when 'home' clicked, use...
+	//when 'home' nav clicked, use...
   $routeProvider.when('/', {
   		templateUrl : 'partials/about.html',
-  		controller : 'homeController'
   	})
   	//when 'new meal' nav clicked, use...
     .when('/details', {
       templateUrl : 'partials/details.html',
-      controller :'detailsController'
+      controller :'mealController'
     })
     //when 'my earnings' nav clicked, use...
     .when('/earnings', {
@@ -22,89 +21,67 @@ app.config(['$routeProvider', function($routeProvider) {
     });
 }]);
 
-/*app.service('sumOfMealsService', function(){
-	//keep meals in an array...
-	var mealsArray = [];
-
-	//keep total value of meals data...
-	var totalData = {
-		meal_count: 0,
-		tip_total: 0,
-		avg_tip: 0.00
-	};
-
-	return {
-		addMeal: function(meal){
-			mealsArray.push(meal);
-			totalData.meal_count ++;
-			totalData.tip_total += customer.subtotal;
-			totalData.avg_tip += (totalData.tip_total/totalData.meal_count)
-		},
-		getMeals: function(){
-			return mealsArray;
-		},
-		getMealsTotalData: function(){
-			return totalData;
-		},
-		resetEverything: function(){
-			mealsArray.length = 0;
-			totalData = {
-				mealsArray: 0,
-				tip_total: 0,
-				avg_tip: 0
-			};
-		}
-	};
-});
-
-app.controller('mealController', function ($scope, sumofMealsService){
-	
-	$scope.meal_count = 1;
-
-	//if submit button is clicked...
-    $scope.submit = function() {
+app.controller('mealController', function($scope, $rootScope){
+    //if submit button is clicked...
+    $scope.submit = function(){
         $scope.submitted = true;
-        $rootScope.$broadcast("calculate", $scope.input);
+        $scope.$broadcast("calculate", $scope.input);
         $scope.submitted = false;
-        resetForm();
+        clear();
+        console.log("submitted!");
     }
-    //if reset button is clicked...
-    $scope.$on("reset", function() {
-        resetForm();
-    });
-    //if cancel button is clicked...
-    $scope.cancel = function() {
-        resetForm();
+    //if clear button is clicked
+    $scope.cancel = function(){
+        clear();
     };
-    //called when reset is clicked...
-    function resetForm() {
-        $scope.input = {
-            meal_price:0,
-            tax_rate:0,
-            tip_percent:0
-        };
-    }
+    //called when reset or clear buttons are clicked
+    function clear() {
+      //clear input form...
+      $scope.input = {
+        meal_price: '',
+        tax_rate: '',
+        tip_percent: ''
+      }
+      //clear customer totals
+      $scope.customer = {
+        subtotal: 0.00,
+        tip: 0.00,
+        total:0.00
+      }
+    };
+
+    //what to do when 'calculate' broadcast is received...
+    $scope.$on("calculate", function(event, data) {
+        $scope.customer.subtotal = data.meal_price + (data.meal_price * (data.tax_rate/100));
+        $scope.customer.tip = data.meal_price * (data.tip_percent/100);
+        $scope.customer.total = $scope.customer.subtotal + $scope.customer.tip;
+        $rootScope.$broadcast("tip_result", $scope.customer);
+    });
 });
 
-app.controller('earningsController', function(){
+app.controller('earningsController', function($scope, $rootScope){
+
 	$scope.earnings= {
-        tip_total:0.00,
-        meal_count:0,
-        avg_tip:0.00
+        tip_total: 0.00,
+        meal_count: 0,
+        avg_tip: 0.00
+  }
+  //when 'tip_result' broadcast received, add to totals
+  $scope.$on("tip_result", function(event, data) {
+      $scope.earnings.tip_total += data.tip;
+      $scope.earnings.meal_count += 1;
+      $scope.earnings.avg_tip += $scope.earnings.tip_total / $scope.earnings.meal_count;
+  });
+  //reset values to zero when user clicks reset button
+  function resetEarnings() {
+    $scope.earnings = {
+      tip_total: 0,
+      meal_count: 0,
+      avg_tip: 0
     }
-    $scope.$on("tip_result", function(event, data) {
-        $scope.earnings.tip_total += data.tip;
-        $scope.earnings.meal_count += 1;
-        $scope.earnings.avg_tip += $scope.earnings.tip_total / $scope.earnings.meal_count;
-    });
-    $scope.$on("reset", function() {
-        $scope.earn= {
-        tip_total:0,
-        meal_count:0,
-        avg_tip:0
-    	}
-    });
-});*/
+  };
+
+});
 
 //how user clicks on nav links are handled...
 app.controller('navigationController', [ '$scope', function($scope, $location) {
